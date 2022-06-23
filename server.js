cac8fb8ce70c754cfbf05979b1c72ebbb4ee0b30
jaytee-thomas <jt.aws.cloud@protonmail.com>
2022-06-23 11:39:49 -0500

    modified server

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ if (process.env.NODE_ENV === 'production') {
   )
 }
 
-const port = process.env.PORT || 3000
-app.listen(port, console.log(`server listing to port 3000 `))
+const port = process.env.PORT || 5000
+app.listen(port, console.log(`server listing to port 5000 `))
 //commit past this point
